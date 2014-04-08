2a3c49bd76e4fdffb986d162596f53f69dddc1d5
kurocon <kevin@kevinalberts.nl>
2014-04-08 10:16:43 +0200

    Implemented multigast

--- src/Network/UDPClient.java
+++ src/Network/UDPClient.java
@@ -6,6 +6,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Sender to send messages from stdin to the multicast group.
  * Created by kevin on 4/7/14.
  */
 public class UDPClient implements Client, Runnable {
@@ -13,41 +14,27 @@ public class UDPClient implements Client, Runnable {
     private static final Logger LOGGER = Logger.getLogger(UDPServer.class.getName());
     private static final int PACKET_SIZE = 1024;
 
-    private DatagramSocket clientSocket = null;
+    private MulticastSocket socket = null;
     private BufferedReader inFromUser = null;
+    private InetAddress group = null;
+    private int port = -1;
 
     private DatagramPacket sendPacket = null;
     private byte[] sendData = new byte[PACKET_SIZE];
 
-    private InetAddress IPAddress = null;
-    private int port = -1;
-
     public boolean should_run = true;
 
-    public UDPClient(String ip, int port){
-        try{
-            this.IPAddress = InetAddress.getByName(ip);
-        } catch (UnknownHostException e) {
-            LOGGER.log(Level.SEVERE, "Could not find the IP address! [" + e.getMessage() + "]");
-            System.exit(1);
-        }
+    public UDPClient(MulticastSocket socket, InetAddress group, int port){
+        this.socket = socket;
+        this.group = group;
         this.port = port;
     }
 
     @Override
     public void run() {
-        LOGGER.log(Level.INFO, "UDPClient is starting...");
-
         inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
-        try {
-            clientSocket = new DatagramSocket();
-        } catch (SocketException e) {
-            LOGGER.log(Level.SEVERE, "Could not open a client socket! ["+e.getMessage()+"]");
-            System.exit(1);
-        }
-
-        LOGGER.log(Level.INFO, "Ready to receive messages!");
+        LOGGER.log(Level.INFO, "Sender is ready to send!");
 
         while(should_run){
 
@@ -58,16 +45,16 @@ public class UDPClient implements Client, Runnable {
                 LOGGER.log(Level.WARNING, "Could not read from stdin! [" + e.getMessage() + "]");
             }
 
-            if(sentence.equals("/exit")){
+            if(sentence != null && sentence.equals("/exit")){
                 this.close();
             }
 
             sendData = sentence.getBytes();
 
-            sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            sendPacket = new DatagramPacket(sendData, sendData.length, this.group, this.port);
 
             try {
-                clientSocket.send(sendPacket);
+                this.socket.send(sendPacket);
             } catch (IOException e) {
                 LOGGER.log(Level.WARNING, "Could not send message to client! [" + e.getMessage() + "]");
             }
@@ -76,15 +63,15 @@ public class UDPClient implements Client, Runnable {
 
     @Override
     public void close(){
-        LOGGER.log(Level.INFO, "Stopping UDPClient...");
+        LOGGER.log(Level.INFO, "Stopping sender...");
 
         this.should_run = false;
-        this.clientSocket.close();
+        this.socket = null;
+
         try {
             this.inFromUser.close();
         } catch (IOException e) {
             LOGGER.log(Level.WARNING, "Could not cleanly close stdin! ["+e.getMessage()+"]");
         }
-        System.exit(0);
     }
 }
--- src/Network/UDPHandler.java
+++ src/Network/UDPHandler.java
@@ -5,6 +5,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Packet handler to handle incoming input packets and forwarding them to the right method in the protocol.
  * Created by kevin on 4/7/14.
  */
 public class UDPHandler implements Handler{
@@ -21,6 +22,10 @@ public class UDPHandler implements Handler{
 
     @Override
     public void handleInput(DatagramPacket packet) {
+        // My god, we received something.
+        // Print it, it might be important!
+        String s = new String(packet.getData());
 
+        LOGGER.log(Level.INFO, "Handler received: " + s);
     }
 }
