9ab8e1215f7b6e49ebfe2163d3d5e70181681482
kurocon <kevin@kevinalberts.nl>
2014-04-10 13:00:27 +0200

    Added stub implementation for File

---
+++ src/network/UDPFile.java
@@ -0,0 +1,36 @@
+package network;
+
+/**
+ * Created by kevin on 4/10/14.
+ */
+public class UDPFile implements File {
+    @Override
+    public void setFilename(String name) {
+
+    }
+
+    @Override
+    public void setData(byte[] data) {
+
+    }
+
+    @Override
+    public void setFiletype(byte[] filetype) {
+
+    }
+
+    @Override
+    public String getFilename() {
+        return null;
+    }
+
+    @Override
+    public byte[] getData() {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] getFiletype() {
+        return new byte[0];
+    }
+}
