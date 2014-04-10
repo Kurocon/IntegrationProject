5a7fb075618c85b653ebabc7e8eee427a6de69ef
kurocon <kevin@kevinalberts.nl>
2014-04-10 11:00:17 +0200

    Fixed TestSecurity for the new non-static Security class

--- src/tests/TestSecurity.java
+++ src/tests/TestSecurity.java
@@ -1,25 +1,24 @@
 package tests;
 
+import network.Security;
+
 public class TestSecurity {
 	
 	public TestSecurity(){
 		byte[] msg = "Hallo, dit is een leuke message".getBytes();
-		byte[] key = new byte[] {0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5};
-		byte[] encrypted = network.Security.encryptData(msg, key);
+		String pw = "ditishetwachtwoord";
+        Security s = new Security();
+        s.setPassword(pw);
+		byte[] encrypted = s.encryptData(msg);
 		System.out.println(byteArrayToHex(msg));
 		System.out.println(byteArrayToHex(encrypted));
-		byte[] decrypted = network.Security.decryptData(encrypted, key);
+		byte[] decrypted = s.decryptData(encrypted);
 		System.out.println(byteArrayToHex(decrypted));
-		
-		
 	}
-	
-	
+
 	public static void main(String[] args){
 		TestSecurity a = new TestSecurity();
 	}
-	
-	
 	
 	public String byteArrayToHex(byte[] a) {
         StringBuilder sb = new StringBuilder();
