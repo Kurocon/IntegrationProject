c41455a062872cf8cee77c0f27839555ee644c96
kurocon <kevin@kevinalberts.nl>
2014-04-10 13:01:22 +0200

    Implemented User

---
+++ src/network/UDPUser.java
@@ -0,0 +1,72 @@
+package network;
+
+import java.net.InetAddress;
+
+/**
+ * Created by kevin on 4/10/14.
+ */
+public class UDPUser implements User {
+
+    private String name;
+    private String hostname;
+    private InetAddress ip;
+    private int port;
+    private Log log;
+    private FileLog files;
+    private long lastSeen;
+
+    public UDPUser(){
+        //this.log = new ChatLog();
+        //this.files = new FileLog();
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setIP(InetAddress ip) {
+        this.ip=ip;
+    }
+
+    @Override
+    public void setPort(int port) {
+        this.port=port;
+    }
+
+    @Override
+    public void logChatMessage(String msg) {
+        this.log.addElement(msg);
+    }
+
+    @Override
+    public void logSpecialContent(String filename, byte[] datatype, byte[] data) {
+        this.files.addElement(filename, datatype, data);
+    }
+
+    @Override
+    public void setLastSeen(long currentTimeAsLong) {
+        this.lastSeen = currentTimeAsLong;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public InetAddress getIP() {
+        return this.ip;
+    }
+
+    @Override
+    public int getPort() {
+        return this.port;
+    }
+
+    @Override
+    public long getLastSeen() {
+        return this.lastSeen;
+    }
+}
