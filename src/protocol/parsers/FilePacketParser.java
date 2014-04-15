5195402d176a7e9e82213352371371beb2ce2a7e
kurocon <kevin@kevinalberts.nl>
2014-04-15 16:33:30 +0200

    Added file packet parser to handle files.

---
+++ src/protocol/parsers/FilePacketParser.java
@@ -0,0 +1,75 @@
+package protocol.parsers;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * FilePacketParser
+ * Created by kevin on 4/15/14.
+ */
+public class FilePacketParser {
+
+    private static final Logger LOGGER = Logger.getLogger(FilePacketParser.class.getName());
+
+    private String fileName;
+    private HashMap<Integer, byte[]> fileMap = new HashMap<>();
+    private int fileLength;
+    private int blockLength;
+    private String savedFilePath;
+
+    public void addFilePacket(PacketParser data) {
+        FileParser fp = new FileParser(data.getData());
+        this.fileName = fp.getFileName();
+        this.fileLength = fp.getTotalPacketsNumber();
+        this.fileMap.put(fp.getCurrentPacketNumber(), fp.getFileData());
+        this.blockLength = fp.getFileData().length;
+
+        if(isFileComplete()){
+            saveFile();
+        }
+    }
+
+    public boolean isFileComplete() {
+        boolean complete = true;
+        for(int i = 0; i < this.fileLength; i++){
+            if(!this.fileMap.containsKey(i)){
+                complete = false;
+            }
+        }
+        return complete;
+    }
+
+    private void saveFile(){
+        int lastPacketLength = this.fileMap.get(fileLength).length;
+        int totalLength = ((this.fileLength * this.blockLength) - (this.blockLength-lastPacketLength));
+        ByteBuffer fileBuffer = ByteBuffer.allocate(totalLength);
+        for(int i = 1; i <= this.fileLength; i++){
+            fileBuffer.put(this.fileMap.get(i));
+        }
+
+        try {
+            Files.write(Paths.get("/tmp/"+fileName), fileBuffer.array());
+        } catch (IOException e) {
+            LOGGER.log(Level.WARNING, "Error while writing output file. ["+e.getMessage()+"]");
+        }
+
+        this.savedFilePath = "/tmp/"+fileName;
+    }
+
+    public String getSavedFilePath(){
+        return this.savedFilePath;
+    }
+
+    public void removeFile() {
+        this.fileMap = new HashMap<>();
+        this.fileName = "";
+        this.fileLength = -1;
+        this.blockLength = -1;
+        this.savedFilePath = "";
+    }
+}
---
+++ src/protocol/parsers/FileParser.java
@@ -0,0 +1,62 @@
+package protocol.parsers;
+
+import java.nio.ByteBuffer;
+
+/**
+ * FileParser
+ * Created by kevin on 4/15/14.
+ */
+public class FileParser extends Parser {
+
+    private int currentPacket;
+    private int totalPackets;
+    private byte[] fileData;
+    private String fileNameAsString;
+    private byte[] fileNameAsBytes;
+    private int filenameLength;
+
+//    CURRENT_SEQ_NUMBER -- 4 bytes
+//    MAX_SEQ_NUMBER -- 4 bytes
+//    FILENAME -- 255 bytes
+//    FILENAME_LENGTH -- 1 byte
+
+    public FileParser(byte[] data) {
+        super(data);
+
+        byte[] curSecNrBytes = new byte[4];
+        byte[] maxSecNrBytes = new byte[4];
+        System.arraycopy(data, 0, curSecNrBytes, 0, 4);
+        System.arraycopy(data, 4, maxSecNrBytes, 0, 4);
+        ByteBuffer curSecNrBuffer = ByteBuffer.allocate(4).put(curSecNrBytes);
+        ByteBuffer maxSecNrBuffer = ByteBuffer.allocate(4).put(maxSecNrBytes);
+        curSecNrBuffer.position(0);
+        maxSecNrBuffer.position(0);
+        this.currentPacket = curSecNrBuffer.getInt();
+        this.totalPackets = maxSecNrBuffer.getInt();
+        this.filenameLength = data[264];
+        this.fileNameAsBytes = new byte[this.filenameLength];
+        System.arraycopy(data, 4, this.fileNameAsBytes, 0, this.filenameLength);
+        this.fileNameAsString = new String(this.fileNameAsBytes);
+
+        this.fileData = new byte[760];
+        System.arraycopy(data, 273, fileData, 0, 760);
+    }
+
+    public byte[] getFileData(){
+        return this.fileData;
+    }
+
+    public int getCurrentPacketNumber(){
+        return this.currentPacket;
+    }
+
+    public int getTotalPacketsNumber(){
+        return this.totalPackets;
+    }
+
+    public String getFileName(){
+        return this.fileNameAsString;
+    }
+
+
+}
