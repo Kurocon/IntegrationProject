092b0fceef334fd4daedc2570858d760d0d7c75a
11gamer11 <j_waals@hotmail.com>
2014-04-15 17:54:34 +0200

    Expended Transfer GUI

---
+++ src/gui/FileController.java
@@ -0,0 +1,61 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+import network.User;
+
+import sampca.SAMPCA;
+
+public class FileController  implements ActionListener {
+
+	private FileChoose view;
+	private SAMPCA client;
+	private JFileChooser fc;
+	private CaController control;
+	private File file;
+
+	public FileController(FileChoose fileChoose, JFileChooser fc, SAMPCA client,CaController control) {
+		this.view = fileChoose;
+		this.client = client;
+		this.fc = fc;
+		this.control = control;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// Handle open button action.
+		if (e.getSource() == view.getOpenButton()) {
+			int returnVal = fc.showOpenDialog(view);
+
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				file = fc.getSelectedFile();
+				// This is where a real application would open the file.
+				view.getTextArea().setText(file.getName());
+			} else {
+				view.getTextArea().setText("Open command cancelled by user.");
+			}
+		}
+		if (e.getSource() == view.getExitButton()) {
+			if (this.view.getExitButton() == e.getSource()) {
+				control.getFrame().dispose();
+			}
+		}
+		if (e.getSource() == view.getTransferButton()) {
+			if(file != null){
+				User user = (User) view.getUserList().getSelectedItem();
+				
+				
+				client.sendFile(file.getAbsolutePath(), user.getIP());
+				control.getFrame().dispose();
+			} else {
+				new Popup("No file selected");
+			}
+		}
+	}
+
+}
